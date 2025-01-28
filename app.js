c843b6c850727d96bd2350cf135907f32af693d4
Juan Carlos <jcarlosgc21@gmail.com>
2025-01-27 22:49:54 -0500

    Se agrego un actualizado automatico de la pagina cada vez que se sortea y se agrego comentarios de lo que hace cada linea

--- app.js
+++ app.js
@@ -17,9 +17,9 @@ function mostrarLista() {
     lista.innerHTML = "<h2><u>Mis amigos</u></h2>";
     // Recorrer la lista y agregar los elementos al contenedor
     for (let i = 0; i < listaAmigos.length; i++) {
-        let item = document.createElement("li"); // Crear un elemento <li>
+        let item = document.createElement("li"); // Crear la lista 
         item.textContent = listaAmigos[i]; // Asignar el valor de la lista al <li>
-        lista.appendChild(item); //Agregar el <li> al contenedor <ul>
+        lista.appendChild(item); //Agregar la lista al contenedor de la lista html, con el DOM
     }
 }
 function limpiarCaja() {
@@ -27,21 +27,26 @@ function limpiarCaja() {
 }
 
 function sortearAmigo() {
-    let listaGanadora= document.querySelector("#resultado");
-    listaGanadora.innerHTML = "";
+    let listaGanadora= document.querySelector("#resultado");//retornarmos el valor a la variable
+    listaGanadora.innerHTML = ""; //ponemos el texto en blanco con cada sorteo
+    //validamos si la lista tiene elementos
     if (listaAmigos.length === 0) {
         alert("¡No hay amigos en la lista para sortear!");
         return;
     }else{
         let indiceAleatorio = Math.floor(Math.random() * listaAmigos.length); // Índice aleatorio
-        let ganador = listaAmigos[indiceAleatorio];
-        console.log(ganador);
-        let resultado = document.createElement("li")
-        resultado.innerHTML = `El Ganador es: ${ganador} 🎉`
-        listaGanadora.appendChild(resultado);
+        let ganador = listaAmigos[indiceAleatorio]; //asignamos el indice a la variavle
+        let resultado = document.createElement("li")//creamos la lista
+        resultado.innerHTML = `El Ganador es: ${ganador} 🎉`//mostramos el texto y motramos al ganador
+        listaGanadora.appendChild(resultado); // 
         limpiarLista();
+        setTimeout(function() {
+            location.reload(); // Esto recarga la página
+        }, 3000);
     }
 }
+
+
 
 function limpiarLista() {
     listaAmigos = []; // Vaciar la lista
