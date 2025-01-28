b4c5e857e0fefe10645995bc356f7d6409f30253
Juan Carlos <jcarlosgc21@gmail.com>
2025-01-27 20:46:45 -0500

    Creación del array y funcion para agregar elementos al array

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let listaAmigo = [];
+
+function agregarAmigo() {
+    let elemento = document.querySelector("#amigo").value;
+    listaAmigo.push(elemento)
+    limpiarCaja();
+    console.log(listaAmigo);
+}
+
+function limpiarCaja() {
+    document.querySelector("#amigo").value = "";
+}
